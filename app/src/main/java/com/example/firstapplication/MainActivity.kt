064f651531bb3323182b64aa594a98efeff1615f
chrisalex18 <caqr970@gmail.com>
2023-04-03 23:32:46 +0200

    small TextViesÂw example

---
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.firstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val text = findViewById<TextView>(R.id.text_view_id)
+        text.setText(R.string.app_name)
+    }
+}
